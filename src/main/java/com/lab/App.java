cc80dbcca04ff56e9ab4c741034c8061cdbe8c9f
2021WA86255 <2021wa86255@wilp.bits-pilani.ac.in>
2025-07-10 17:32:31 +0530

    Triggered CI by changing print message

--- src/main/java/com/lab/App.java
+++ src/main/java/com/lab/App.java
@@ -2,6 +2,6 @@ package com.lab;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI!");
+        System.out.println("Change detected by Jenkins CI!");
     }
 }
